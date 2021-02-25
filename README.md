d63c912c375f2b4c7c52e68c9658afa9d5aaf62f
Brayam Valero <bvalerop@gmail.com>
2021-02-25 16:11:40 -0500

    Feat: Adding Extra Endpoints, fixing README & adding .env.example

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Soraka
+# Soraka - Node.js Wrapper
+
+Soraka is a Node.js Wrapper inspired by [Irelia](https://github.com/perezpaya/irelia), it basically allows you to fetch data easilly from the oficial [League of Legends API](http://developer.riotgames.com)
+
+In order to get your Riot API Key, you need create an account at [http://developer.riotgames.com](http://developer.riotgames.com)
+
+### Usage
+
+```javascript
+const Soraka = require("./controller");
+
+const soraka = new Soraka({
+  secure: true,
+  host: "api.riotgames.com",
+  apiKey: "Your API Key",
+});
+
+// [getChampionByName] => Get Champion Information By Name
+soraka.getChampionByName("lan", "Soraka", (err, res) => console.log(res));
+```
+
+### Extra Information
+
+To be Defined :)
--- controller.js
+++ controller.js
@@ -65,52 +65,270 @@ module.exports = class Soraka {
     }
   }
 
-  // [Method] => Get Data Dragon Version Based on Region Selected
+  /** [DataDragon] Get Data Dragon Version Based on Region Selected */
   getDataDragonVersion(region, callback) {
     const url = `https://ddragon.leagueoflegends.com/realms/${region}.json`;
     return this.makeRequest(url, callback);
   }
 
-  /**
-   * Get All Champions
-   * @method
-   * @param {string} region - The region from which is getting called.
-   * @param {callback} callback - The callback function.
-   */
+  /** [Champions] Get All Champions */
   async getChampions(region, callback) {
-    // First we get the current DataDragon Version
     const { cdn, l: lang, v: ver } = await this.getDataDragonVersion(region);
-    // Then we format the url based on result
     const result = `${cdn}/${ver}/data/${lang}/champion.json`;
-    // Finally, we return the result
     return this.makeRequest(result, callback);
   }
 
-  // [Method] => Return the Summoner Information
+  /** [Champions] Get (1) Champion By Name */
+  async getChampionByName(region, name, callback) {
+    const { cdn, l: lang, v: ver } = await this.getDataDragonVersion(region);
+    const result = `${cdn}/${ver}/data/${lang}/champion/${name}.json`;
+    return this.makeRequest(result, callback);
+  }
+
+  /** [Third Party] Get a summoner by account ID */
+  getThirdPartyCode(region, encryptedSummonerId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/platform/v4/third-party-code/by-summoner/${encryptedSummonerId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Summoner] Get a summoner by account ID */
+  getSummonerByAccountId(region, encryptedAccountId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/summoner/v4/summoners/by-account/${encryptedAccountId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Summoner] Get a summoner by summoner name */
   getSummonerByName(region, summonerName, callback) {
     const url = this.generateUrl({
       region,
-      path: "/lol/summoner/v4/summoners/by-name/" + summonerName,
+      path: `/lol/summoner/v4/summoners/by-name/${summonerName}`,
     });
     return this.makeRequest(url, callback);
   }
 
-  // [Method] => Return All Champion Masteries from the Summoner
+  /** [Summoner] Get a summoner by PUUID */
+  getSummonerByPUUID(region, encryptedPUUID, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/summoner/v4/summoners/by-puuid/${encryptedPUUID}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Summoner] Get a summoner by summoner ID */
+  getSummonerByPUUID(region, encryptedSummonerId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/summoner/v4/summoners/${encryptedSummonerId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Summoner] Get league entries in all queues for a given summoner ID */
+  getSummonerLeagues(region, encryptedSummonerId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/league/v4/entries/by-summoner/${encryptedSummonerId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Mastery] Get all champion mastery entries sorted by number of champion points descending */
   getAllChampionMasteries(region, encryptedSummonerId, callback) {
     const url = this.generateUrl({
       region,
-      path:
-        "/lol/champion-mastery/v4/champion-masteries/by-summoner/" +
-        encryptedSummonerId,
+      path: `/lol/champion-mastery/v4/champion-masteries/by-summoner/${encryptedSummonerId}`,
     });
     return this.makeRequest(url, callback);
   }
 
-  // [Method] => Return Weekly Champion Rotation Based on Region
+  /** [Mastery] Get a champion mastery by player ID and champion ID */
+  getChampionMastery(region, encryptedSummonerId, championId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/champion-mastery/v4/champion-masteries/by-summoner/${encryptedSummonerId}/by-champion/${championId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Mastery] Get a player's total champion mastery score, which is the sum of individual champion mastery levels */
+  getAllChampionMasteriesScore(region, encryptedSummonerId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/champion-mastery/v4/scores/by-summoner/${encryptedSummonerId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Rotations] Returns champion rotations, including free-to-play and low-level free-to-play rotations */
   getChampionRotations(region, callback) {
     const url = this.generateUrl({
       region,
       path: "/lol/platform/v3/champion-rotations",
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Clash] Get players by summoner ID */
+  getClashPlayer(region, summonerId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/clash/v1/players/by-summoner/${summonerId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Clash] Get players by summoner ID */
+  getClashTeam(region, teamId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/clash/v1/teams/${teamId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Clash] Get all active or upcoming tournaments */
+  getClashTournaments(region, callback) {
+    const url = this.generateUrl({
+      region,
+      path: "/lol/clash/v1/tournaments",
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Clash] Get tournament by Team ID */
+  getClashTournamentTeamId(region, teamId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/clash/v1/tournaments/by-team/${teamId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Clash] Get tournament by Tournament ID */
+  getClashTournamentId(region, tournamentId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/clash/v1/tournaments/${tournamentId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Queue] Get the challenger league for given queue */
+  getChallengerLeagues(region, queue, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/league/v4/challengerleagues/by-queue/${queue}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Queue] Get all the league entries */
+  getAllLeagues(region, queue, tier, division, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/league/v4/entries/${queue}/${tier}/${division}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Queue] Get the grandmaster league of a specific queue */
+  getGrandMasterLeagues(region, queue, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/league/v4/grandmasterleagues/by-queue/${queue}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Queue] Get league with given ID, including inactive entries */
+  getLeagueId(region, leagueId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/league/v4/leagues/${leagueId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Queue] Get the master league for given queue */
+  getMasterLeagues(region, queue, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/league/v4/masterleagues/by-queue/${queue}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Match] Get match by match id */
+  getMatchByMatchId(region, matchId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/match/v4/matches/${matchId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Match] Get matchlist for games played on given account ID and platform ID and filtered using given filter parameters, if any */
+  getMatchListByAccountId(region, encryptedAccountId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/match/v4/matchlists/by-account/${encryptedAccountId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Match] Get match timeline by match ID */
+  getMatchTimelineByMatchId(region, matchId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/match/v4/timelines/by-match/${matchId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Match] Get match IDs by tournament code */
+  getMatchIdsByTournamentCode(region, tournamentCode, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/match/v4/matches/by-tournament-code/${tournamentCode}/ids`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Match] Get match by match ID and tournament code */
+  getMatchByMatchIdAndTournamentCode(
+    region,
+    matchId,
+    tournamentCode,
+    callback
+  ) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/match/v4/matches/${matchId}/by-tournament-code/${tournamentCode}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Spectator] Get current game information for the given summoner ID */
+  getActiveSummonerGame(region, encryptedSummonerId, callback) {
+    const url = this.generateUrl({
+      region,
+      path: `/lol/spectator/v4/active-games/by-summoner/${encryptedSummonerId}`,
+    });
+    return this.makeRequest(url, callback);
+  }
+
+  /** [Spectator] Get list of featured games */
+  getFeaturedGames(region, callback) {
+    const url = this.generateUrl({
+      region,
+      path: "/lol/spectator/v4/featured-games",
     });
     return this.makeRequest(url, callback);
   }
